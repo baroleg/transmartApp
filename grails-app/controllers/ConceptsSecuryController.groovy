a9aba2951deb86b489feeac51fdca14a1f82b04f
mirasrael <d_signer@mail.ru>
2014-06-19 16:18:39 +0400

    Improve available accessions query

--- grails-app/controllers/ConceptsSecuryController.groovy
+++ grails-app/controllers/ConceptsSecuryController.groovy
@@ -24,21 +24,24 @@ class ConceptsSecuryController {
         if(!i2b2HelperService.isAdmin(user)) {
             def userid = user?.id
             groovy.sql.Sql sql = new groovy.sql.Sql(dataSource)
-            def accessionSQLSet = sql.rows("select \n" +
-                    "be.accession " +
-                    "from \n" +
-                    "searchapp.search_auth_sec_object_access sasoa,\n" +
-                    "searchapp.search_sec_access_level ssal,\n" +
-                    "searchapp.search_secure_object sso,\n" +
-                    "biomart.bio_experiment be\n" +
-                    "where\n" +
-                    "ssal.access_level_value > 0\n" +
-                    "and sasoa.secure_access_level_id = ssal.search_sec_access_level_id\n" +
-                    "and (sasoa.auth_principal_id = ? or\n" +
-                    "sasoa.auth_principal_id in ( " +
-                    "select sagm.auth_group_id from searchapp.search_auth_group_member sagm where sagm.auth_user_id = ?))" +
-                    "and sasoa.secure_object_id = sso.search_secure_object_id\n" +
-                    "and sso.bio_data_id = be.bio_experiment_id", userid, userid)
+            def accessionSQLSet = sql.rows("""
+                select
+                  distinct be.accession
+                from
+                  searchapp.search_auth_sec_object_access sasoa
+                    left join searchapp.search_auth_group_member sagm
+                    on sasoa.auth_principal_id = sagm.auth_group_id,
+                  searchapp.search_sec_access_level ssal,
+                  searchapp.search_secure_object sso,
+                  biomart.bio_experiment be
+                  where
+                    ssal.access_level_value > 0
+                    and sasoa.secure_access_level_id = ssal.search_sec_access_level_id
+                    and coalesce(sagm.auth_user_id, sasoa.auth_principal_id) = ?
+                    and sasoa.secure_object_id = sso.search_secure_object_id
+                    and sso.bio_data_id = be.bio_experiment_id
+            """, userid)
+            accession = accessionSQLSet.accession
         }
 
         children = children.findAll {

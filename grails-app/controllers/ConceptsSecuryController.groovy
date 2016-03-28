16926394e2025eb7752123361089d63e5f1ce68e
Alexander <alexander.omelchenko@thomsonreuters.com>
2016-03-28 14:58:20 +0400

    Show associated files in tree ( for 1.2.4 version)

--- grails-app/controllers/ConceptsSecuryController.groovy
+++ grails-app/controllers/ConceptsSecuryController.groovy
@@ -1,3 +1,4 @@
+import fm.FmFolderAssociation
 import grails.converters.JSON
 
 /**
@@ -16,15 +17,19 @@ class ConceptsSecuryController {
 
     def getChildren() {
         def parentConceptKey = params.get('concept_key')
-        def parent = conceptsResourceService.getByKey(parentConceptKey)
-        def children = parent.children
-        def user = springSecurityService.getPrincipal()
-        def accession = children*.sourcesystemCd
 
-        if(!i2b2HelperService.isAdmin(user)) {
-            def userid = user?.id
-            groovy.sql.Sql sql = new groovy.sql.Sql(dataSource)
-            def accessionSQLSet = sql.rows("""
+        def filesIndexOf = parentConceptKey.lastIndexOf('Files\\')
+        def result
+        if (filesIndexOf != parentConceptKey.length() - 6) {
+            def parent = conceptsResourceService.getByKey(parentConceptKey)
+            result = parent.children
+            def user = springSecurityService.getPrincipal()
+            def accession = result*.sourcesystemCd
+
+            if(!i2b2HelperService.isAdmin(user)) {
+                def userid = user?.id
+                groovy.sql.Sql sql = new groovy.sql.Sql(dataSource)
+                def accessionSQLSet = sql.rows("""
                 select
                   distinct be.accession
                 from
@@ -41,14 +46,64 @@ class ConceptsSecuryController {
                     and sasoa.secure_object_id = sso.search_secure_object_id
                     and sso.bio_data_id = be.bio_experiment_id
             """, userid)
-            accession = accessionSQLSet.accession
+                accession = accessionSQLSet.accession
+            }
+
+            result = result.findAll {
+                accession.contains(it.sourcesystemCd)
+            }
+
+            if (parent.cVisualattributes == 'FAS') {
+                def fmFolderAssociation = FmFolderAssociation.findByObjectUid("EXP:${parent.sourcesystemCd}")
+                def filesList = fmFolderAssociation?.fmFolder?.fmFiles
+                result.add(new resJSON(
+                        key: parent.key + 'Files\\',
+                        level: parent.level + 1,
+                        fullName: parent.fullName + 'Files\\',
+                        name: 'Files',
+                        tooltip: parent.tooltip + 'Files\\',
+                        visualAttributes: ["FOLDER", "ACTIVE"],
+                        metadata: "",
+                        dimensionCode: parent.dimensionCode + 'Files\\',
+                        dimensionTableName: "FILE"
+                ))
+            }
+        } else {
+            parentConceptKey = ((String)parentConceptKey).substring(0, filesIndexOf)
+            def parent = conceptsResourceService.getByKey(parentConceptKey)
+            result = []
+            if (parent.cVisualattributes == 'FAS') {
+                def fmFolderAssociation = FmFolderAssociation.findByObjectUid("EXP:${parent.sourcesystemCd}")
+                def filesList = fmFolderAssociation?.fmFolder?.fmFiles
+
+                filesList.each {
+                    result.add(new resJSON(
+                            key: parent.key + 'Files\\' + it.displayName,
+                            level: parent.level + 2,
+                            fullName: parent.fullName + 'Files\\' + it.displayName,
+                            name: it.displayName,
+                            tooltip: parent.tooltip + 'Files\\' + it.displayName,
+                            visualAttributes: ["FILE", "ACTIVE"],
+                            metadata: "{fileId: ${it.id}, fileType: '${it.fileType}'}",
+                            dimensionCode: parent.dimensionCode + 'Files\\' + it.displayName,
+                            dimensionTableName: "FILE"
+                    ))
+                }
+            }
         }
 
-        children = children.findAll {
-            accession.contains(it.sourcesystemCd)
-        }
-
-        render children as JSON
+        render result as JSON
     }
 
+    static class resJSON{
+        String key;
+        String level;
+        String fullName;
+        String name;
+        String tooltip;
+        List   visualAttributes;
+        String metadata;
+        String dimensionCode;
+        String dimensionTableName;
+    }
 }
